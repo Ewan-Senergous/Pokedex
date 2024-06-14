2178206e3bdacf295f6649fe6ffbf49081082aab
Your Name <ewan16270409@outlook.fr>
2024-06-14 11:52:49 +0200

    quete 3

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,10 +1,27 @@
-function PokemonCard () {
+function PokemonCard() {
+    const pokemonList = [
+      {
+        name: "bulbasaur",
+        imgSrc:
+          "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+      },
+      {
+        name: "mew",
+      },
+    ];
+  
+    const pokemon = pokemonList[0];
+  
     return (
-        <figure>
-            <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt="Bulbasaur" />
-            <figcaption>"Bulbasaur"</figcaption>
-        </figure>
-    );
-}
-
-export default PokemonCard
+      <figure>
+        <figcaption>{pokemon.name}</figcaption>
+  
+        {pokemon.imgSrc ? 
+        (<img src={pokemon.imgSrc} 
+            alt={pokemon.name} />) 
+            : (<p>???</p>)}
+            </figure> 
+  ); 
+  }
+  
+  export default PokemonCard;
