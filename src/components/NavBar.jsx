a28bc953dc5f0bf44f7066a1ea457dbb60d71ebb
Your Name <ewan16270409@outlook.fr>
2024-06-30 23:37:13 +0200

    7ème quete React Basics

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,24 +1,26 @@
 import PropTypes from "prop-types";
+
 function NavBar({ pokemonIndex, setPokemonIndex, pokemonList }) {
+  const handlePreviusClick = () => {
+    pokemonIndex > 0 ? setPokemonIndex(pokemonIndex - 1) : "";
+  };
+  const handleNextClick = () => {
+    pokemonIndex < pokemonList.length - 1
+      ? setPokemonIndex(pokemonIndex + 1)
+      : null;
+  };
   return (
-    <div>
-      {pokemonIndex > 0 ? (
-        <button onClick={() => setPokemonIndex(pokemonIndex - 1)}>
-          {" "}
-          Précédent
-        </button>
-      ) : (
-        ""
-      )}
-      {pokemonIndex < pokemonList.length - 1 ? (
-        <button onClick={() => setPokemonIndex(pokemonIndex + 1)}>
-          {" "}
-          Suivant
-        </button>
-      ) : (
-        ""
-      )}
-    </div>
+    <header>
+      <button onClick={handlePreviusClick} disabled={pokemonIndex === 0}>
+        Précédent
+      </button>
+      <button
+        onClick={handleNextClick}
+        disabled={pokemonIndex === pokemonList.length - 1}
+      >
+        Suivant
+      </button>
+    </header>
   );
 }
 NavBar.propTypes = {
