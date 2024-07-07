3dbc8db082cd640a2d21da2cb0e4439d73e5587f
Your Name <ewan16270409@outlook.fr>
2024-07-07 15:47:02 +0200

    React basics quete 8 bis

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,15 +1,22 @@
 import PropTypes from "prop-types";
 function NavBar({ setPokemonIndex, pokemonList }) {
+  const handlePokemonClick = (index) => {
+    setPokemonIndex(index);
+    if (pokemonList[index].name === "pikachu") {
+      alert("pika pikachu !!!");
+    }
+  };
   return (
     <header>
       {pokemonList.map((pokemon, index) => (
-        <button key={index} onClick={() => setPokemonIndex(index)}>
+        <button key={pokemon.name} onClick={() => handlePokemonClick(index)}>
           {pokemon.name}
         </button>
       ))}
     </header>
   );
 }
+
 NavBar.propTypes = {
   setPokemonIndex: PropTypes.func.isRequired,
   pokemonList: PropTypes.array.isRequired,
